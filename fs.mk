a343d5211659983400b3df975abd6f1bc712ee8e
lePerdu <zdpeltzer@gmail.com>
2018-03-13 15:47:37 -0400

    Add basename() and dirname()
    Changed string.z80 to utils.z80

--- fs.mk
+++ fs.mk
@@ -5,7 +5,8 @@
 FS_SRC = fs-src
 FS = fs
 
-FS_BINS := $(addprefix $(FS)/bin/, init echo pwd ls cat link unlink rmdir rm mv)
+FS_BINS := $(addprefix $(FS)/bin/, init echo pwd ls cat link unlink rmdir \
+	rm mv basename dirname)
 
 $(FS): $(FS_BINS)
 	@touch $@
@@ -13,16 +14,18 @@ $(FS): $(FS_BINS)
 $(FS)/bin:
 	@mkdir -p $@
 
-$(FS)/bin/init: $(FS_SRC)/sh.z80 $(FS_SRC)/string.z80
-$(FS)/bin/echo: $(FS_SRC)/echo.z80 $(FS_SRC)/string.z80
-$(FS)/bin/pwd: $(FS_SRC)/pwd.z80 $(FS_SRC)/string.z80
-$(FS)/bin/ls: $(FS_SRC)/ls.z80 $(FS_SRC)/string.z80
-$(FS)/bin/cat: $(FS_SRC)/cat.z80 $(FS_SRC)/string.z80
-$(FS)/bin/link: $(FS_SRC)/link.z80 $(FS_SRC)/string.z80
-$(FS)/bin/unlink: $(FS_SRC)/unlink.z80 $(FS_SRC)/string.z80
-$(FS)/bin/rmdir: $(FS_SRC)/rmdir.z80 $(FS_SRC)/string.z80
-$(FS)/bin/mv: $(FS_SRC)/mv.z80 $(FS_SRC)/string.z80
-$(FS)/bin/rm: $(FS_SRC)/rm.z80 $(FS_SRC)/string.z80
+$(FS)/bin/init: $(FS_SRC)/sh.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/echo: $(FS_SRC)/echo.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/pwd: $(FS_SRC)/pwd.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/ls: $(FS_SRC)/ls.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/cat: $(FS_SRC)/cat.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/link: $(FS_SRC)/link.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/unlink: $(FS_SRC)/unlink.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/rmdir: $(FS_SRC)/rmdir.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/mv: $(FS_SRC)/mv.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/rm: $(FS_SRC)/rm.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/dirname: $(FS_SRC)/dirname.z80 $(FS_SRC)/utils.z80
+$(FS)/bin/basename: $(FS_SRC)/basename.z80 $(FS_SRC)/utils.z80
 
 $(FS_BINS): | $(FS)/bin
 	$(SPASM) $(SPASMOPTS) $< $@
